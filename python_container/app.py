3e4211def1ca2671fa74ef9d09c7c8325512c61e
s-gap-ado-aks-bronga <s-gap-ado-aks-GITHUB-DG@gap.com>
2022-02-21 21:43:26 -0800

    [LP] Updated Data

--- python_container/app.py
+++ python_container/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, jsonify
-from time
+from flask import Flask, jsonify, render_template
+import time, random
 
 app = Flask(__name__)
 
+# list of cat images
+
+images = [
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    "https://thumbs.dreamstime.com/z/i-love-coffee-13785741.jpg"
+]
+
+
 app.route("/")
-def hello_world():
-    return jsonify({"Time Right Now": time.time()})
+def index():
+    url = random.choice(images)
+    return render_template('index.html',url=url)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
